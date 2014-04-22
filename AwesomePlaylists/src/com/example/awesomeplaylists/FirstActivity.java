5432b40819f779a2f326687acaf2bea6300fe375
shaimr <shaimr@gmail.com>
2014-04-22 22:04:16 +0300

    First Activity - is example activity,
    main activity is the start page for now...

--- AwesomePlaylists/src/com/example/awesomeplaylists/FirstActivity.java
+++ AwesomePlaylists/src/com/example/awesomeplaylists/FirstActivity.java
@@ -1,15 +1,9 @@
 package com.example.awesomeplaylists;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 public class FirstActivity extends Activity {
 
--- AwesomePlaylists/src/com/example/awesomeplaylists/MainActivity.java
+++ AwesomePlaylists/src/com/example/awesomeplaylists/MainActivity.java
@@ -1,15 +1,9 @@
 package com.example.awesomeplaylists;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 // moo
 
@@ -21,11 +15,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
 	}
 
 	@Override
@@ -47,22 +36,4 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
-
 }
