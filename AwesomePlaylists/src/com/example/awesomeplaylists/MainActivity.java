906fe9eacace8f11e7bc5964b31eefebd9c14f3a
shaimr <shaimr@gmail.com>
2014-04-27 11:27:42 +0300

    User Settings

--- AwesomePlaylists/src/com/example/awesomeplaylists/MainActivity.java
+++ AwesomePlaylists/src/com/example/awesomeplaylists/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.awesomeplaylists;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private static final int RESULT_SETTINGS = 1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,10 +33,20 @@ public class MainActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
+//		int id = item.getItemId();
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
+		
+        switch (item.getItemId()) {
+        
+        case R.id.action_settings:
+            Intent i = new Intent(this, SettingsActivity.class);
+            startActivityForResult(i, RESULT_SETTINGS);
+            break;
+ 
+        }
+		
 		return super.onOptionsItemSelected(item);
 	}
 			
@@ -45,4 +60,38 @@ public class MainActivity extends Activity {
 		Intent intent = new Intent(this, PlayItActivity.class);					
 		startActivity(intent);	
 	}	
+	
+	public void settingsClicked(View view) {
+		Intent intent = new Intent(this, UserSettingsActivity.class);					
+		startActivity(intent);	
+	}	
+	
+   @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+ 
+        switch (requestCode) {
+        case RESULT_SETTINGS:
+            showUserSettings();
+            break;
+ 
+        }
+ 
+    }
+	
+    private void showUserSettings() {
+        SharedPreferences sharedPrefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
+ 
+        StringBuilder builder = new StringBuilder();
+ 
+        builder.append("\n Username: "
+                + sharedPrefs.getString("prefUsername", "NULL"));
+ 
+        builder.append("\n Send report:"
+                + sharedPrefs.getBoolean("prefSendReport", false));
+ 
+        builder.append("\n Sync Frequency: "
+                + sharedPrefs.getString("prefSyncFrequency", "NULL"));
+    }
 }
